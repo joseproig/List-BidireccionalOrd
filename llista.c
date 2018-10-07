b32ae304bbe41c0b9a5f0bc4aea8566ade4f08e6
joseproig <josep.roig@students.salle.url.edu>
2018-10-07 21:22:43 +0200

    doc: Added new coments to llista.c
    New explanatory comments about the code in llista.c

--- llista.c
+++ llista.c
@@ -5,9 +5,12 @@
 Llista llistaBIORDcrea () {
 	Llista l;
 	l.pri = (Node*) malloc (sizeof (Node));
+	//Comprovem que no hi hagi cap error en la creacio en el malloc
 	if ( l.pri != NULL) {
 		l.ult = (Node*) malloc (sizeof(Node));
+		//Tornem a comprovar-ho
 		if (l.ult != NULL) {
+			//Els punters dels fantasmes que no entrin dins la llista els fiquem a NULL
 			(*(l.pri)).ant = NULL;
 			(*(l.ult)).seg = NULL;
 			l.pdi = l.pri;
@@ -16,19 +19,22 @@ Llista llistaBIORDcrea () {
 		}
 		else {
 			printf ("\nError de memoria");
+			//Com que s'haura creat el malloc de l.pri pero no el de l.ult l'eliminem
 			free (l.pri);		
 		}
 	}
 	else {
 		printf ("\nError de memoria");	
 	}
+	//Retornem la llista creada
 	return l;
 }
 void llistaBIORDdestrueix (Llista * l) {
         Node * aux;
-        aux = (Node*) malloc (sizeof(Node));
+	aux = (Node*) malloc (sizeof(Node));
         if (aux != NULL) {
-                while ((*l).pri != NULL) {
+                //Anem eliminant les diferents posicions de la llista, des del principi, fins arribar al final
+		while ((*l).pri != NULL) {
                         aux = (*l).pri;
                         (*l).pri = (*(*l).pri).seg;
                         free (aux);
@@ -39,17 +45,21 @@ void llistaBIORDdestrueix (Llista * l) {
         }
 }
 void llistaBIORDvesInici (Llista * l) {
+	//El principi passara a ser la posicio posterior al nostre fantasma que es la que hi ha informacio
 	(*l).pdi = (*(*l).pri).seg;
 }
 void llistaBIORDvesFinal (Llista * l) {
+	//El final passara a ser la posicio anterior al nostre fantasma que es la que hi ha informacio
 	(*l).pdi = (*(*l).ult).ant;
 }
 void llistaBIORDesborra (Llista * l) {
 	Node * aux;
 	aux = (Node*) malloc (sizeof(Node));
 	if  (aux != NULL) {
+		//Esborrarem caselles sempre i quan aquestes no siguin els fantasmes
 		if ( (*l).pdi != (*l).pri && (*l).pdi != (*l).ult) { 
 			aux = (*l).pdi;
+			//Fem que la posicio anterior al PDI i la seguent s'enllacin
 			(*((*(*l).pdi).ant)).seg = (*(*l).pdi).seg;
 			(*((*(*l).pdi).seg)).ant = (*(*l).pdi).ant;
 			(*l).pdi = (*(*l).pdi).seg;
@@ -64,7 +74,8 @@ void llistaBIORDesborra (Llista * l) {
 	}
 }
 void llistaBIORDavanca (Llista * l) {
-        if ( (*(*l).pdi).seg != (*l).ult && (*(*l).pdi).seg != NULL) {
+        //Avancarem sempre i quan la seguent posicio no sigui un fantasma
+	if ( (*(*l).pdi).seg != (*l).ult && (*(*l).pdi).seg != NULL) {
                 (*l).pdi = (*(*l).pdi).seg;
         }
         else {
@@ -72,7 +83,8 @@ void llistaBIORDavanca (Llista * l) {
         }
 } 
 void llistaBIORDretrocedeix (Llista * l) {
-        if ((*(*l).pdi).ant != (*l).pri && (*(*l).pdi).ant != NULL) {
+        //Retrocedirem, sempre i quan la posicio anterior no sigui un fantasma
+	if ((*(*l).pdi).ant != (*l).pri && (*(*l).pdi).ant != NULL) {
                 (*l).pdi = (*(*l).pdi).ant;
         }
         else {
@@ -80,6 +92,7 @@ void llistaBIORDretrocedeix (Llista * l) {
         }
 }
 Element llistaBIORDconsulta (Llista  l) {
+	//Retornme el valor del PDI
 	return (*l.pdi).e;
 
 }
@@ -88,10 +101,13 @@ void llistaBIORDinsereixOrdenada (Llista * l, Element e) {
 	int trobat;
 	aux = (Node*) malloc (sizeof (Node));
 	if (aux != NULL) {
+		//Copiem la informacio a la variable auxiliar
 		(*aux).e.edat = e.edat;
 		strcpy ((*aux).e.nom_persona, e.nom_persona);
+		//Passem el nostre PDI a la seguent
 		(*l).pdi = (*(*l).pri).seg;
 		trobat = 0;
+		//Fins que no arribem al fantasma utlim o no trobessim un major, anirem passant
 		while ((*l).pdi != (*l).ult && !trobat) {
 			if ((*(*l).pdi).e.edat > e.edat) { 
 				trobat = 1;
@@ -100,21 +116,26 @@ void llistaBIORDinsereixOrdenada (Llista * l, Element e) {
 				(*l).pdi = (*(*l).pdi).seg;
 			}
 		}
+		//El lloc on haurem sortit sera el que anira despres de la nova inclusio, per tant crearem un vincle entre l'anterior del PDI actual, la nova inclusió i el PDI 
 		(*aux).ant = (*(*l).pdi).ant; 
 		(*aux).seg = (*l).pdi;
 		(*(*(*l).pdi).ant).seg = aux;
 		(*(*l).pdi).ant = aux;
+		//Un cop passada la informacio, el que fem es passar el PDI a la nova inclusió
 		(*l).pdi = aux;
 	}
 
 }
 int llistaBIORDfi (Llista  l) {
+	//Comprovem que no sigui el final, osigui que el seguent del PDI apunti al fantasma 
 	return (*l.pdi).seg == l.ult;
 }
 int llistaBIORDbuida (Llista l) {
+	//Comprovem que el primer fantasma no apunti al segon
 	return (*l.pri).seg == l.ult;
 }
 int llistaBIORDinici (Llista l) {
+	//Comprovem qsi el seguent del nostre primer fantasma es el nostre PDI
 	return l.pdi == (*l.pri).seg;
 }
 
